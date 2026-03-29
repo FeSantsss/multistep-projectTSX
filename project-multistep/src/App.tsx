86353f1c0f767507216c8271d73c0866f0d6ecfe
Felipy <tercetime18@gmail.com>
2026-03-29 18:01:31 -0300

    style: form base

--- project-multistep/src/App.tsx
+++ project-multistep/src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container"></div>
+
+      <p>Created by</p>
+      <h2>Felipy Santos</h2>
+    </div>
+  );
 }
 
 export default App;
